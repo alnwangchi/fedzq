f0ec8843b83e617b685f722bf04ace6401ff8a73
alnwangchi <alnwangchi@gmail.com>
2024-04-19 19:37:31 +0800

    feat: add report PDF download function

--- app/(client)/interview/report/page.tsx
+++ app/(client)/interview/report/page.tsx
@@ -2,10 +2,71 @@
 
 import { usePDF } from 'react-to-pdf';
 import { Button } from '@/components/ui/button';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { useInterview } from '@/Providers/InterviewContext';
 
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
 const page = () => {
-  return <Content />;
+  const interviewData: any = useInterview();
+  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
+
+  const downloadPDF = () => {
+    const capture = document.querySelector('#pdf') as HTMLElement;
+
+    html2canvas(capture).then((canvas) => {
+      const imgData = canvas.toDataURL('img/png');
+      const doc = new jsPDF('p', 'mm', 'a4');
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
+      doc.save('report.pdf');
+    });
+  };
+  return (
+    <>
+      <div>
+        <div className='py-14 px-[10%] aspect-[210/297] w-full' ref={targetRef} id='pdf'>
+          <div className='grid grid-cols-2 grid-rows-auto gap-5'>
+            <div className='space-y-3'>
+              <h3>Javascript</h3>
+              {Object.entries(interviewData?.javascript).map(([key, value], idx) => {
+                return <QA key={key} q={key} a={value as string} />;
+              })}
+            </div>
+            <div className='space-y-3'>
+              <h3>React</h3>
+              {Object.entries(interviewData?.react).map(([key, value], idx) => {
+                return <QA key={key} q={key} a={value as string} />;
+              })}
+            </div>
+          </div>
+        </div>
+        <div className='fixed bottom-5 right-5 space-x-4'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button onClick={() => toPDF()}>Download PDF</Button>
+              </TooltipTrigger>
+              {/* not sure why the default align = center not working */}
+              <TooltipContent className='translate-x-[-16px]'>
+                <p>use react-to-pdf</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button onClick={downloadPDF}>Download PDF</Button>
+              </TooltipTrigger>
+              <TooltipContent className='translate-x-[-16px]'>
+                <p>use jspdf</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default page;
@@ -18,31 +79,3 @@ const QA = ({ q, a }: { q: string; a: string }) => (
     </div>
   </div>
 );
-
-const Content = () => {
-  const interviewData: any = useInterview();
-  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
-  return (
-    <div>
-      <div className='p-10' ref={targetRef}>
-        <div className='grid grid-cols-2 grid-rows-auto gap-5'>
-          <div className='space-y-3'>
-            <h3>Javascript</h3>
-            {Object.entries(interviewData?.javascript).map(([key, value], idx) => {
-              return <QA key={key} q={key} a={value as string} />;
-            })}
-          </div>
-          <div className='space-y-3'>
-            <h3>React</h3>
-            {Object.entries(interviewData?.react).map(([key, value], idx) => {
-              return <QA key={key} q={key} a={value as string} />;
-            })}
-          </div>
-        </div>
-      </div>
-      <Button className='fixed bottom-5 right-5' onClick={() => toPDF()}>
-        Download PDF
-      </Button>
-    </div>
-  );
-};
