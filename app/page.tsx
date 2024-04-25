93040ccdfbb2259ed2e22e6259f26d0341e0948b
alnwangchi <alnwangchi@gmail.com>
2024-04-25 21:32:32 +0800

    chore:  首頁未做前先直接導到登入頁

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image';
-
+import { redirect } from 'next/navigation';
 export default function Home() {
-  return <main className='flex min-h-screen flex-col items-center justify-between'>home</main>;
+  // 首頁未做前先直接導到登入頁
+  redirect('/login');
+  return <main className='flex min-h-screen flex-col items-center justify-between'>Hello</main>;
 }
