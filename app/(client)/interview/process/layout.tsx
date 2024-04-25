538c7b10706a7c7f193d157fb7fd00e46587ad90
alnwangchi <alnwangchi@gmail.com>
2024-04-26 00:45:56 +0800

    fix: removing unnecessary layout

--- app/(client)/interview/process/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <section className='pt-5'>{children}</section>;
-};
-
-export default layout;
--- app/(client)/interview/report/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <section>{children}</section>;
-};
-
-export default layout;
