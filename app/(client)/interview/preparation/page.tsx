b358e91ee3b629f44c556271c5299bb02331e6ff
alnwangchi <alnwangchi@gmail.com>
2024-05-01 19:00:09 +0800

    fear: add toggle set in local storage switch

--- app/(client)/interview/preparation/page.tsx
+++ app/(client)/interview/preparation/page.tsx
@@ -1,19 +1,33 @@
 'use client';
-import QCard from '@/components/interview/QCard';
+import { usePreparation, usePreparationDispatch } from '@/Providers/PreparationContext';
 import QYouShouldAsk from '@/components/interview/QYouShouldAsk';
 import QYouWillBeAsked from '@/components/interview/QYouWillBeAsked';
-import Qpersonal from '@/components/interview/Qpersonal';
 import ButtonShapeTabs from '@/components/local/ButtonShapeTabs';
-import { Separator } from '@/components/ui/separator';
+import { Switch } from '@/components/ui/switch';
 import { useState } from 'react';
 
 const tabs = ["You'll Be Asked", 'You Should Ask'];
 
 const page = () => {
   const [selected, setSelected] = useState(tabs[0]);
+
+  const preparation = usePreparation();
+  const { isSaveInLocalStorage } = preparation as any;
+
+  const dispatch: any = usePreparationDispatch();
+
   return (
     <section>
-      <ButtonShapeTabs selected={selected} setSelected={setSelected} tabs={tabs} />
+      <div className='flex justify-between items-start'>
+        <ButtonShapeTabs selected={selected} setSelected={setSelected} tabs={tabs} />
+        <div className='flex justify-center items-center gap-3'>
+          <span className='text-sm'>Save In LocalStorage </span>
+          <Switch
+            checked={isSaveInLocalStorage}
+            onCheckedChange={() => dispatch({ type: 'toggleIsSaveInLocalStorage' })}
+          />
+        </div>
+      </div>
 
       {selected === tabs[0] && <QYouWillBeAsked />}
       {selected === tabs[1] && <QYouShouldAsk />}
